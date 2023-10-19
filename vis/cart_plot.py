bccb8af34dbacb889792faa4c16fe0b9305cc207
raychew <chew@iau.uni-frankfurt.de>
2023-10-19 14:08:02 +0200

    cart_plot.py: added delaunay error plotting function
    will fine tune this a bit as a next step

--- vis/cart_plot.py
+++ vis/cart_plot.py
@@ -44,7 +44,7 @@ def lat_lon_delaunay(topo, tri, levels, fs=(8,4),   \
                      highlight_indices = [44,45, 88,89, 16,17], \
                      fn = '../output/delaunay.pdf', \
                      output_fig = False, \
-                     int = 1
+                     int = 1, \
                      ):
     plt.figure(figsize=fs)
 
@@ -54,13 +54,15 @@ def lat_lon_delaunay(topo, tri, levels, fs=(8,4),   \
     points = tri.points
 
     cbar = plt.colorbar(im,fraction=0.2,pad=0.005, shrink=1.0)
+
     plt.triplot(points[:,0], points[:,1], tri.simplices, c='C7', lw=0.5)
+
+
     plt.plot(points[:,0], points[:,1], 'wo', ms=2.0)
     # plt.plot(tri_clons, tri_clats, 'rx', ms=4.0)
 
-    highlight_indices = np.array(highlight_indices)
-
     if label_idxs:
+        highlight_indices = np.array(highlight_indices)
         tri_indices = np.arange(len(tri.tri_lat_verts))
 
         for idx in tri_indices:
@@ -78,6 +80,82 @@ def lat_lon_delaunay(topo, tri, levels, fs=(8,4),   \
     plt.tight_layout()
     if output_fig: plt.savefig(fn)
     plt.show()
+
+
+def error_delaunay(topo, tri, fs=(8,4),   \
+                     label_idxs = False, \
+                     highlight_indices = [44,45, 88,89, 16,17], \
+                     fn = '../output/delaunay.pdf', \
+                     output_fig = False, \
+                     iint = 1, \
+                     errors = None, \
+                     alpha_max = 0.5,
+                  ):
+    fig = plt.figure(figsize=fs)
+    ax = plt.axes(projection=ccrs.PlateCarree())
+
+    ax.coastlines()
+    ax.contourf(topo.lon_grid[::iint], topo.lat_grid[::iint], topo.topo[::iint],
+                alpha=1.0,
+                transform=ccrs.PlateCarree(),
+                cmap='binary',
+                )
+    
+    points = tri.points
+
+    from matplotlib.colors import ListedColormap
+    cmap = plt.cm.RdYlGn
+    my_cmap = cmap(np.arange(cmap.N))
+
+
+    zeros_len = 30 # must be even
+    lcmap_ov2 = cmap.N / 2
+    my_cmap[:,-1] = np.concatenate((np.linspace(0, alpha_max, int(lcmap_ov2 - zeros_len/2))[::-1], np.zeros(zeros_len), np.linspace(0, alpha_max, int(lcmap_ov2 - zeros_len/2))))
+    my_cmap = ListedColormap(my_cmap)
+
+    im = ax.tripcolor(points[:,0], points[:,1], tri.simplices.copy(), facecolors=errors, edgecolors='k', cmap=my_cmap, alpha=None)
+
+    if label_idxs:
+        highlight_indices = np.array(highlight_indices)
+        tri_indices = np.arange(len(tri.tri_lat_verts))
+
+        for idx in tri_indices:
+            colour = 'C7'
+            fw = None
+            
+            if (idx in highlight_indices) or (idx in highlight_indices+1):
+                colour='C3'
+                fw = 'bold'
+        
+            ax.annotate(tri_indices[idx], (tri.tri_clons[idx],tri.tri_clats[idx]), (tri.tri_clons[idx]-0.3,tri.tri_clats[idx]-0.2), c=colour, fontweight=fw)
+
+    cax = fig.add_axes([0.99, 0.22, 0.025, 0.55])
+    fig.colorbar(im, cax=cax)
+
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                    linewidth=2, color='gray', alpha=0.0, linestyle='--')
+    gl.top_labels = False
+    gl.left_labels = False
+
+    gl.xlocator = LongitudeLocator()
+    gl.ylocator = LatitudeLocator()
+    gl.xformatter = LongitudeFormatter(auto_hide=False)
+    gl.yformatter = LatitudeFormatter()
+
+    ax.tick_params(axis="both",
+                tickdir='out',
+                length=15,
+                grid_transform=ccrs.PlateCarree())
+
+    
+    plt.xlabel("longitude [deg.]")
+    plt.ylabel("latitude [deg.]")
+    plt.tight_layout()
+    if output_fig: plt.savefig(fn)
+
+    plt.show()
+    
+                     
 
 def lat_lon_icon(topo, triangles, \
                  fs=(10,6), \
