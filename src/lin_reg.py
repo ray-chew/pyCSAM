a5655867954e42244336c9c91ffc5a173a3de8e5
raychew <chew@iau.uni-frankfurt.de>
2023-11-07 19:40:57 +0100

    lin_reg.py: removed deprecated feature with get_n_modes

--- src/lin_reg.py
+++ src/lin_reg.py
@@ -49,7 +49,7 @@ def do(fobj, cell, lmbda = 0.0):
     return a_m, data_recons
 
 
-def do_iter(fobj, cell, lmbda = 0.0, get_n_modes=False):
+def do_iter(fobj, cell, lmbda = 0.0):
     if fobj.grad:
         cell.get_grad()
         data = cell.grad_topo_m
@@ -73,11 +73,6 @@ def do_iter(fobj, cell, lmbda = 0.0, get_n_modes=False):
 
     a_m, _ = gmres(E_tilda_lm, h_tilda_l)
 
-    if get_n_modes:
-        # to be replaced with params.n_modes
-        a_m[np.where(a_m < np.sort(a_m.ravel())[::-1][99])] = 0.0
-
-    print( (a_m > 0.0).sum() )
 
     # regular FFT considers normalization by total nu  mber of datapoints N=100
     # so multiply the Fourier coefficients by N here
