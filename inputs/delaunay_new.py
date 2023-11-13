9098bb7f386dca549595776af5a8fe5661bf8e8d
raychew <chew@iau.uni-frankfurt.de>
2023-11-13 20:35:12 +0100

    delaunay_new.py: reimplemented the refinement infrastructure; refactored code by moving stuff to interface.py and diagnostics.py

--- inputs/delaunay_new.py
+++ inputs/delaunay_new.py
@@ -10,7 +10,7 @@ import importlib
 import matplotlib.pyplot as plt
 
 from src import io, var, utils, fourier, physics, delaunay
-from runs import interface
+from runs import interface, diagnostics
 from vis import plotter, cart_plot
 
 %load_ext autoreload
@@ -33,12 +33,11 @@ topo = var.topo_cell()
 
 # read grid
 reader = io.ncdata(padding=params.padding, padding_tol=(60-params.padding))
+reader.read_dat(params.fn_grid, grid)
+grid.apply_f(utils.rad2deg) 
 
 # writer object
 writer = io.writer(params.output_fn, params.rect_set, debug=params.debug_writer)
-
-reader.read_dat(params.fn_grid, grid)
-grid.apply_f(utils.rad2deg) 
 
 # we only keep the topography that is inside this lat-lon extent.
 lat_verts = np.array(params.lat_extent)
@@ -58,6 +57,7 @@ tri = delaunay.get_decomposition(topo, xnp=params.delaunay_xnp, ynp=params.delau
 writer.write_all('decomposition', tri)
 writer.populate('decomposition', 'rect_set', params.rect_set)
 
+
 # %%
 if params.run_full_land_model:
     params.rect_set = delaunay.get_land_cells(tri, topo, height_tol=0.5)
@@ -65,6 +65,7 @@ if params.run_full_land_model:
 
 params_orig = deepcopy(params)
 writer.write_all_attrs(params)
+
 # %%
 # Plot the loaded topography...
 %autoreload
@@ -79,19 +80,23 @@ cart_plot.lat_lon_delaunay(topo, tri, levels, label_idxs=True, fs=(20,12), highl
 
 # %%
 %autoreload
-pmf_diff = []
-pmf_refs = []
-pmf_sums = []
-pmf_fas  = []
-pmf_ssums= []
-idx_name = []
 
 nhi = params.nhi
 nhj = params.nhj
 
+fa = interface.first_appx(nhi, nhj, params, topo)
+sa = interface.second_appx(nhi, nhj, params, topo, tri)
+# diagnostics object
+diag = diagnostics.delaunay_metrics(params, tri, writer=writer)
+dplot = diagnostics.diag_plotter(params, nhi, nhj)
+
+if not params.no_corrections:
+    rel_errs_orig = []
+
 for rect_idx in params.rect_set:
 
     #################################################
+    #
     # compute DFFT over reference quadrilateral cell.
     #
     print("computing reference quadrilateral cell: ", (rect_idx, rect_idx+1))
@@ -109,18 +114,25 @@ for rect_idx in params.rect_set:
     ref_run = interface.get_pmf(nhi,nhj,params.U,params.V)
     ampls_ref, uw_ref, fft_2D_ref, kls_ref = ref_run.dfft(cell_ref)
 
+    if params.debug_writer:
+        writer.populate(rect_idx, 'topo_ref', cell_ref.topo)
+        writer.populate(rect_idx, 'spectrum_ref', ampls_ref)
+        writer.populate(rect_idx, 'pmf_ref', uw_ref)
+
     v_extent = [fft_2D_ref.min(), fft_2D_ref.max()]
+    sols = (cell_ref, ampls_ref, uw_ref, fft_2D_ref)
+    dplot.show(rect_idx, sols, kls=kls_ref, v_extent = v_extent, dfft_plot=True)
 
-    if params.plot:
-        fs = (15,5.0)
-        fig, axs = plt.subplots(1,3, figsize=fs)
-        fig_obj = plotter.fig_obj(fig, nhi, nhj)
-        axs[0] = fig_obj.phys_panel(axs[0], fft_2D_ref, title='T%i + T%i: Reference FFT reconstruction' %(rect_idx, rect_idx+1), xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell_ref.lon.min(), cell_ref.lon.max(), cell_ref.lat.min(), cell_ref.lat.max()], v_extent=v_extent)
+    # if params.plot:
+    #     fs = (15,5.0)
+    #     fig, axs = plt.subplots(1,3, figsize=fs)
+    #     fig_obj = plotter.fig_obj(fig, nhi, nhj)
+    #     axs[0] = fig_obj.phys_panel(axs[0], fft_2D_ref, title='T%i + T%i: Reference FFT reconstruction' %(rect_idx, rect_idx+1), xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell_ref.lon.min(), cell_ref.lon.max(), cell_ref.lat.min(), cell_ref.lat.max()], v_extent=v_extent)
 
-        axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_ref, kls_ref[0], kls_ref[1], typ='real')
-        axs[2] = fig_obj.fft_freq_panel(axs[2], uw_ref, kls_ref[0], kls_ref[1], title="FFT PMF spectrum", typ='real')
-        plt.tight_layout()
-        plt.show()
+    #     axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_ref, kls_ref[0], kls_ref[1], typ='real')
+    #     axs[2] = fig_obj.fft_freq_panel(axs[2], uw_ref, kls_ref[0], kls_ref[1], title="FFT PMF spectrum", typ='real')
+    #     plt.tight_layout()
+    #     plt.show()
 
 
     ###################################
@@ -135,144 +147,222 @@ for rect_idx in params.rect_set:
 
         cell_fa = cell_ref
     else:
-        cell_fa = var.topo_cell()
+        cell_fa, ampls_fa, uw_fa, dat_2D_fa = fa.do(simplex_lat, simplex_lon)
 
-        if params.taper_fa:
-            interface.taper_quad(params, simplex_lat, simplex_lon, cell_fa, topo)
-        else:
-            utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell_fa, topo, rect=params.rect)    
+    diag.update_quad(rect_idx, uw_ref, uw_fa)
 
-        first_guess = interface.get_pmf(nhi,nhj,params.U,params.V)
+    if params.debug_writer:
+        writer.populate(rect_idx, 'spectrum_fg', ampls_fa)
+        writer.populate(rect_idx, 'recon_fg', dat_2D_fa)
+        writer.populate(rect_idx, 'pmf_fg', uw_fa)
 
-        ampls_fa, uw_fa, dat_2D_fa = first_guess.sappx(cell_fa, lmbda=params.lmbda_fa, iter_solve=params.fa_iter_solve)
+    sols = (cell_fa, ampls_fa, uw_fa, dat_2D_fa)
+    dplot.show(rect_idx, sols, v_extent=v_extent)
 
-    if params.plot:
-        fs = (15.0,4.0)
-        fig, axs = plt.subplots(1,3, figsize=fs)
-        fig_obj = plotter.fig_obj(fig, nhi, nhj)
-        axs[0] = fig_obj.phys_panel(axs[0], dat_2D_fa, title='T%i+T%i: FF reconstruction' %(rect_idx,rect_idx+1), xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell_fa.lon.min(), cell_fa.lon.max(), cell_fa.lat.min(), cell_fa.lat.max()], v_extent=v_extent)
+    # if params.plot:
+    #     fs = (15.0,4.0)
+    #     fig, axs = plt.subplots(1,3, figsize=fs)
+    #     fig_obj = plotter.fig_obj(fig, nhi, nhj)
+    #     axs[0] = fig_obj.phys_panel(axs[0], dat_2D_fa, title='T%i+T%i: FF reconstruction' %(rect_idx,rect_idx+1), xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell_fa.lon.min(), cell_fa.lon.max(), cell_fa.lat.min(), cell_fa.lat.max()], v_extent=v_extent)
 
-        if params.dfft_first_guess:
-            axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_fa, kls_fa[0], kls_fa[1], typ='real')
-            axs[2] = fig_obj.fft_freq_panel(axs[2], uw_fa, kls_fa[0], kls_fa[1], title="PMF spectrum", typ='real')
-        else:
-            axs[1] = fig_obj.freq_panel(axs[1], ampls_fa)
-            axs[2] = fig_obj.freq_panel(axs[2], uw_fa, title="PMF spectrum")
+    #     if params.dfft_first_guess:
+    #         axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_fa, kls_fa[0], kls_fa[1], typ='real')
+    #         axs[2] = fig_obj.fft_freq_panel(axs[2], uw_fa, kls_fa[0], kls_fa[1], title="PMF spectrum", typ='real')
+    #     else:
+    #         axs[1] = fig_obj.freq_panel(axs[1], ampls_fa)
+    #         axs[2] = fig_obj.freq_panel(axs[2], uw_fa, title="PMF spectrum")
 
-        plt.tight_layout()
-        plt.show()
+    #     plt.tight_layout()
+    #     plt.show()
     
+
+    ###################################
+    #
+    # Do second approximation over non-
+    # quadrilateral grid cells
+    # 
     triangle_pair = np.zeros(2, dtype='object')
+
     for cnt, idx in enumerate(range(rect_idx, rect_idx+2)):
-        # make a copy of the spectrum obtained from the FA.
-        fq_cpy = np.copy(ampls_fa)
-        fq_cpy[np.isnan(fq_cpy)] = 0.0 # necessary. Otherwise, popping with fq_cpy.max() gives the np.nan entries first.
+        
+        cell, ampls_sa, uw_sa, dat_2D_sa = sa.do(idx, ampls_fa)
 
-        cell = var.topo_cell()
-
-        print("computing idex: ", idx)
-
-        simplex_lat = tri.tri_lat_verts[idx]
-        simplex_lon = tri.tri_lon_verts[idx]
-
-        # use the non-quadrilateral topography
-        utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, topo, rect=True)
-        utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, topo, rect=False, filtered=False)
-
-        if params.taper_sa:
-            interface.taper_nonquad(params, simplex_lat, simplex_lon, cell, topo)
-        # else:
-
-        #     utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, topo, rect=False, filtered=False)
-
-        second_guess = interface.get_pmf(nhi,nhj,params.U,params.V)
-
-        indices = []
-        modes_cnt = 0
-        while modes_cnt < params.n_modes:
-            max_idx = np.unravel_index(fq_cpy.argmax(), fq_cpy.shape)
-            # skip the k = 0 column
-            # if max_idx[1] == 0:
-            #     fq_cpy[max_idx] = 0.0
-            # # else we want to use them
-            # else:
-            indices.append(max_idx)
-            fq_cpy[max_idx] = 0.0
-            modes_cnt += 1
-
-        if not params.cg_spsp:
-            k_idxs = [pair[1] for pair in indices]
-            l_idxs = [pair[0] for pair in indices]
-
-        if params.dfft_first_guess:
-            second_guess.fobj.set_kls(k_idxs, l_idxs, recompute_nhij = True, components='real')
-        else:
-            second_guess.fobj.set_kls(k_idxs, l_idxs, recompute_nhij = False)
-
-        ampls_sa, uw_sa, dat_2D_sa = second_guess.sappx(cell, lmbda=params.lmbda_sa, updt_analysis=True, scale=1.0, iter_solve=params.sa_iter_solve)      
-
-        if params.plot:
-            fs = (15,4.0)
-            fig, axs = plt.subplots(1,3, figsize=fs)
-            fig_obj = plotter.fig_obj(fig, nhi, nhj)
-            axs[0] = fig_obj.phys_panel(axs[0], dat_2D_sa, title='T%i: Reconstruction' %idx, xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell.lon.min(), cell.lon.max(), cell.lat.min(), cell.lat.max()], v_extent=v_extent)
-            if params.dfft_first_guess:
-                axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_sa, kls_fa[0], kls_fa[1], typ='real')
-                axs[2] = fig_obj.fft_freq_panel(axs[2], uw_sa, kls_fa[0], kls_fa[1], title="PMF spectrum", typ='real')
-            else:
-                axs[1] = fig_obj.freq_panel(axs[1], ampls_sa)
-                axs[2] = fig_obj.freq_panel(axs[2], uw_sa, title="PMF spectrum")
-            plt.tight_layout()
-            # plt.savefig('../output/T%i.pdf' %idx)
-            plt.show()
+        sols = (cell, ampls_sa, uw_sa, dat_2D_sa)
+        dplot.show(idx, sols, v_extent=v_extent)
+        # if params.plot:
+        #     fs = (15,4.0)
+        #     fig, axs = plt.subplots(1,3, figsize=fs)
+        #     fig_obj = plotter.fig_obj(fig, nhi, nhj)
+        #     axs[0] = fig_obj.phys_panel(axs[0], dat_2D_sa, title='T%i: Reconstruction' %idx, xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell.lon.min(), cell.lon.max(), cell.lat.min(), cell.lat.max()], v_extent=v_extent)
+        #     if params.dfft_first_guess:
+        #         axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_sa, kls_fa[0], kls_fa[1], typ='real')
+        #         axs[2] = fig_obj.fft_freq_panel(axs[2], uw_sa, kls_fa[0], kls_fa[1], title="PMF spectrum", typ='real')
+        #     else:
+        #         axs[1] = fig_obj.freq_panel(axs[1], ampls_sa)
+        #         axs[2] = fig_obj.freq_panel(axs[2], uw_sa, title="PMF spectrum")
+        #     plt.tight_layout()
+        #     # plt.savefig('../output/T%i.pdf' %idx)
+        #     plt.show()
 
         cell.uw = uw_sa
         triangle_pair[cnt] = cell
+
+        writer.write_all(idx, cell, cell.analysis)
+        writer.populate(idx, 'pmf_sg', uw_sa)
         del cell
 
-    uw_0 = triangle_pair[0].uw.sum()
-    uw_1 = triangle_pair[1].uw.sum()
-    uw_sum = uw_0 + uw_1
+    ###################################
+    #
+    # Do iterative refinement?
+    # 
 
-    ampls_0 = triangle_pair[0].analysis.ampls
-    ampls_1 = triangle_pair[1].analysis.ampls
-    ampls_sum = (ampls_0 + ampls_1)
-    analysis_sum = triangle_pair[0].analysis
-    analysis_sum.ampls = ampls_sum
+    ref_topo = np.copy(cell_ref.topo)
+    topo_sum = np.zeros_like(ref_topo)
+    rel_err = diag.get_rel_err(triangle_pair)
+    rel_errs_orig.append(rel_err)
+    print(rel_err)
+    print(diag)
+    corrected = False
 
-    ideal = physics.ideal_pmf(U=params.U, V=params.V)
-    uw_spec_sum = 0.5 * ideal.compute_uw_pmf(analysis_sum)
+    # while (rel_err > 0.2) and (not params.no_corrections):
+    #     sa.n_modes = int(sa.n_modes / 2)
+ 
+    #     print("correcting overestimation... with n_modes=", sa.n_modes)
 
-    print(uw_0, uw_1)
-    print(uw_ref.sum(), uw_fa.sum(), uw_sum, uw_spec_sum)
+    #     refinement_pair = np.zeros(2, dtype='object')
 
-    pmf_refs.append(uw_ref.sum())
-    pmf_fas.append(uw_fa.sum())
-    pmf_sums.append(uw_sum)
-    pmf_ssums.append(uw_spec_sum)
+    #     cell_fa, ampls_fa, uw_fa, dat_2D_fa = fa.do(simplex_lat, simplex_lon)
 
-writer.populate('decomposition', 'pmf_diff', pmf_diff)
+    #     v_extent = [dat_2D_fa.min(), dat_2D_fa.max()]
+
+    #     if params.plot:
+    #         fs = (15.0,4.0)
+    #         fig, axs = plt.subplots(1,3, figsize=fs)
+    #         fig_obj = plotter.fig_obj(fig, nhi, nhj)
+    #         axs[0] = fig_obj.phys_panel(axs[0], dat_2D_fa, title='T%i+T%i: FF reconstruction' %(rect_idx,rect_idx+1), xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell_fa.lon.min(), cell_fa.lon.max(), cell_fa.lat.min(), cell_fa.lat.max()], v_extent=v_extent)
+
+    #         if params.dfft_first_guess:
+    #             axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_fa, kls_fa[0], kls_fa[1], typ='real')
+    #             axs[2] = fig_obj.fft_freq_panel(axs[2], uw_fa, kls_fa[0], kls_fa[1], title="PMF spectrum", typ='real')
+    #         else:
+    #             axs[1] = fig_obj.freq_panel(axs[1], ampls_fa)
+    #             axs[2] = fig_obj.freq_panel(axs[2], uw_fa, title="PMF spectrum")
+
+    #         plt.tight_layout()
+    #         plt.show()
+
+    #     for cnt, idx in enumerate(range(rect_idx, rect_idx+2)):
+
+    #         cell, ampls_rf, uw_rf, dat_2D_rf = sa.do(idx, ampls_fa)
+
+    #         cell.uw = uw_pmf_refined
+    #         refinement_pair[cnt] = cell
+
+    #         if params.plot:
+    #             fs = (15,4.0)
+    #             fig, axs = plt.subplots(1,3, figsize=fs)
+    #             fig_obj = plotter.fig_obj(fig, nhi, nhj)
+    #             axs[0] = fig_obj.phys_panel(axs[0], dat_2D_rf, title='T%i: Reconstruction' %idx, xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell.lon.min(), cell.lon.max(), cell.lat.min(), cell.lat.max()], v_extent=v_extent)
+    #             if params.dfft_first_guess:
+    #                 axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_rf, kls_fa[0], kls_fa[1], typ='real')
+    #                 axs[2] = fig_obj.fft_freq_panel(axs[2], uw_rf, kls_fa[0], kls_fa[1], title="PMF spectrum", typ='real')
+    #             else:
+    #                 axs[1] = fig_obj.freq_panel(axs[1], ampls_rf)
+    #                 axs[2] = fig_obj.freq_panel(axs[2], uw_rf, title="PMF spectrum")
+    #             plt.tight_layout()
+    #             # plt.savefig('../output/T%i.pdf' %idx)
+    #             plt.show()
+
+
+    #     corrected = True
+    #     rel_err = diag.get_rel_err(refinement_pair)
+    #     print(rel_err)
+    #     print(diag)
+
+    # sa.n_modes = params.n_modes
+        
+    
+    while np.abs(rel_err) > 0.2 and (not params.no_corrections): 
+        print("correcting underestimation... with n_modes=", sa.n_modes)
+
+        refinement_pair = np.zeros(2, dtype='object')
+
+        sa.params.lmbda_sa = 1e-1
+
+        topo_sum += dat_2D_fa
+        res_topo = -np.sign(rel_err) * (ref_topo - topo_sum)
+        res_topo -= res_topo.mean()
+        # ref_topo = np.copy(res_topo)
+
+        cell_fa, ampls_fa, uw_fa, dat_2D_fa = fa.do(simplex_lat, simplex_lon, res_topo=res_topo)
+
+        v_extent = [dat_2D_fa.min(), dat_2D_fa.max()]
+
+        for cnt, idx in enumerate(range(rect_idx, rect_idx+2)):
+            # res_topo = cell_ref.topo - triangle_pair[cnt].analysis.recon
+
+            cell, ampls_rf, uw_rf, dat_2D_rf = sa.do(idx, ampls_fa, res_topo = res_topo)
+
+            ampls_sum = triangle_pair[cnt].analysis.ampls - np.sign(rel_err) * ampls_rf
+
+            cutoff = np.sort(ampls_sum.ravel())[::-1][params.n_modes-1]
+            ampls_sum[np.where(ampls_sum < cutoff)] = 0.0
+
+            print((ampls_sum > 0.0).sum())
+
+            cell.analysis.ampls = ampls_sum
+            triangle_pair[cnt].analysis.ampls = ampls_sum
+
+            ideal = physics.ideal_pmf(U=params.U, V=params.V)
+            uw_pmf_refined = ideal.compute_uw_pmf(cell.analysis, summed=True)
+
+            print("uw_pmf_refined", uw_pmf_refined)
+
+            cell.uw = uw_pmf_refined
+            refinement_pair[cnt] = cell
+
+            sols = (cell, ampls_rf, uw_rf, dat_2D_rf)
+            dplot.show(idx, sols, v_extent=v_extent)
+
+            # if params.plot:
+            #     fs = (15,4.0)
+            #     fig, axs = plt.subplots(1,3, figsize=fs)
+            #     fig_obj = plotter.fig_obj(fig, nhi, nhj)
+            #     axs[0] = fig_obj.phys_panel(axs[0], dat_2D_rf, title='T%i: Reconstruction' %idx, xlabel='longitude [km]', ylabel='latitude [km]', extent=[cell.lon.min(), cell.lon.max(), cell.lat.min(), cell.lat.max()], v_extent=v_extent)
+            #     if params.dfft_first_guess:
+            #         axs[1] = fig_obj.fft_freq_panel(axs[1], ampls_rf, kls_fa[0], kls_fa[1], typ='real')
+            #         axs[2] = fig_obj.fft_freq_panel(axs[2], uw_rf, kls_fa[0], kls_fa[1], title="PMF spectrum", typ='real')
+            #     else:
+            #         axs[1] = fig_obj.freq_panel(axs[1], ampls_rf)
+            #         axs[2] = fig_obj.freq_panel(axs[2], uw_rf, title="PMF spectrum")
+            #     plt.tight_layout()
+            #     # plt.savefig('../output/T%i.pdf' %idx)
+            #     plt.show()
+
+        corrected = True
+        rel_err = diag.get_rel_err(refinement_pair)
+        print(rel_err)
+        print(diag)
+
+        # sa.n_modes /= 2
+        sa.params.lmbda_sa = 1e-1
+
+    if corrected:
+        triangle_pair = refinement_pair
+    # print(rel_err)
+
+    diag.update_pair(triangle_pair)
+
+
+diag.end(verbose=True)
+
+
 
 # %%
-def get_rel_diff(arr, ref):
-    arr = np.array(arr)
-    ref = np.array(ref)
-
-    return arr / ref - 1.0
-
-
-def get_max_diff(arr, ref, max):
-    arr = np.array(arr)
-    ref = np.array(ref)
-
-    return (arr - ref) / max
-
-sum_diff = get_max_diff(pmf_sums, pmf_refs, np.array(pmf_refs).max())
-sum_diff = get_rel_diff(pmf_sums, pmf_refs)
-
-
-pmf_percent_diff = np.array(sum_diff) * 100
-plotter.error_bar_plot(params.rect_set, pmf_percent_diff, params, gen_title=True)
+print(rel_errs_orig)
+print(diag.rel_errs)
+%autoreload
+plotter.error_bar_plot(params.rect_set, diag.rel_errs, params, comparison=np.array(rel_errs_orig)*100, gen_title=True)
 
 
 
@@ -280,13 +370,12 @@ plotter.error_bar_plot(params.rect_set, pmf_percent_diff, params, gen_title=True
 importlib.reload(io)
 importlib.reload(cart_plot)
 
-
 errors = np.zeros((len(tri.simplices)))
 errors[:] = np.nan
 errors[params.rect_set] = pmf_percent_diff
 errors[np.array(params.rect_set)+1] = pmf_percent_diff
 
 levels = np.linspace(-1000.0, 3000.0, 5)
-cart_plot.error_delaunay(topo, tri, label_idxs=True, fs=(12,8), highlight_indices=params.rect_set, output_fig=False, iint=1, errors=errors, alpha_max=0.6)
+cart_plot.error_delaunay(topo, tri, label_idxs=False, fs=(12,8), highlight_indices=params.rect_set, output_fig=False, iint=1, errors=errors, alpha_max=0.6)
 
 # %%
--- runs/interface.py
+++ runs/interface.py
@@ -106,24 +106,27 @@ class get_pmf(object):
     
 
 
-def taper_quad(params, simplex_lat, simplex_lon, cell, topo):
+def taper_quad(params, simplex_lat, simplex_lon, cell, topo, res_topo=None):
     # get quadrilateral mask
     utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, topo, rect=True)
 
     # get tapered mask with padding
     taper = utils.taper(cell, params.padding, art_it=params.taper_art_it)
-    taper.do_tapering()    
+    taper.do_tapering()
 
     # get tapered topography in quadrilateral with padding
     utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, topo, rect=True, padding=params.padding, topo_mask=taper.p)
 
-def taper_nonquad(params, simplex_lat, simplex_lon, cell, topo):
+def taper_nonquad(params, simplex_lat, simplex_lon, cell, topo, res_topo=None):
     # get tapered mask with padding
     taper = utils.taper(cell, params.padding, art_it=params.taper_art_it)
     taper.do_tapering()
 
     # get padded topography
     utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, topo, rect=True, padding=params.padding)
+    
+    if res_topo is not None:
+        cell.topo = res_topo
 
     # get padded topography in non-quad
     utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, topo, rect=False, padding=params.padding, filtered=False)
@@ -136,4 +139,86 @@ def taper_nonquad(params, simplex_lat, simplex_lon, cell, topo):
     # cell.topo = taper.p * cell.topo * mask
     # cell.mask = mask 
 
-    
+
+class first_appx(object):
+
+    def __init__(self, nhi, nhj, params, topo):
+        self.nhi, self.nhj = nhi, nhj
+        self.params = params
+        self.topo = topo
+
+    def do(self, simplex_lat, simplex_lon, res_topo=None):
+        cell_fa = var.topo_cell()
+
+        if res_topo is None:
+            if self.params.taper_fa:
+                taper_quad(self.params, simplex_lat, simplex_lon, cell_fa, self.topo, res_topo)
+            else:
+                utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell_fa, self.topo, rect=self.params.rect)    
+        else:
+            cell_fa.topo = res_topo
+            utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell_fa, self.topo, padding=self.params.padding, rect=False, mask=np.ones_like(res_topo).astype(bool))    
+
+        first_guess = get_pmf(self.nhi,self.nhj,self.params.U,self.params.V)
+
+        ampls_fa, uw_fa, dat_2D_fa = first_guess.sappx(cell_fa, lmbda=self.params.lmbda_fa, iter_solve=self.params.fa_iter_solve)
+        return cell_fa, ampls_fa, uw_fa, dat_2D_fa
+
+class second_appx(object):
+
+    def __init__(self, nhi,nhj, params, topo, tri):
+        self.params = params
+        self.topo = topo
+        self.tri = tri
+        self.nhi, self.nhj = nhi, nhj
+        self.n_modes = params.n_modes
+
+    def do(self, idx, ampls_fa, res_topo=None):
+
+        # make a copy of the spectrum obtained from the FA.
+        fq_cpy = np.copy(ampls_fa)
+        fq_cpy[np.isnan(fq_cpy)] = 0.0 # necessary. Otherwise, popping with fq_cpy.max() gives the np.nan entries first.
+
+        cell = var.topo_cell()
+
+        simplex_lat = self.tri.tri_lat_verts[idx]
+        simplex_lon = self.tri.tri_lon_verts[idx]
+
+        # use the non-quadrilateral self.topography
+        utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, self.topo, rect=True)
+
+        if (res_topo is not None) and (not self.params.taper_sa):
+            cell.topo = res_topo * cell.mask
+
+        utils.get_lat_lon_segments(simplex_lat, simplex_lon, cell, self.topo, rect=False, filtered=False)
+
+        if self.params.taper_sa:
+            taper_nonquad(self.params, simplex_lat, simplex_lon, cell, self.topo, res_topo=res_topo)
+
+        second_guess = get_pmf(self.nhi,self.nhj,self.params.U,self.params.V)
+
+        indices = []
+        modes_cnt = 0
+        while modes_cnt < self.n_modes:
+            max_idx = np.unravel_index(fq_cpy.argmax(), fq_cpy.shape)
+            # skip the k = 0 column
+            # if max_idx[1] == 0:
+            #     fq_cpy[max_idx] = 0.0
+            # # else we want to use them
+            # else:
+            indices.append(max_idx)
+            fq_cpy[max_idx] = 0.0
+            modes_cnt += 1
+
+        if not self.params.cg_spsp:
+            k_idxs = [pair[1] for pair in indices]
+            l_idxs = [pair[0] for pair in indices]
+
+        if self.params.dfft_first_guess:
+            second_guess.fobj.set_kls(k_idxs, l_idxs, recompute_nhij = True, components='real')
+        else:
+            second_guess.fobj.set_kls(k_idxs, l_idxs, recompute_nhij = False)
+
+        ampls_sa, uw_sa, dat_2D_sa = second_guess.sappx(cell, lmbda=self.params.lmbda_sa, updt_analysis=True, scale=1.0, iter_solve=self.params.sa_iter_solve)      
+        
+        return cell, ampls_sa, uw_sa, dat_2D_sa
