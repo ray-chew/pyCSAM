820fb7c8631580cb0dabcbc97aad9ac17a1b973e
raychew <chew@iau.uni-frankfurt.de>
2023-11-13 20:37:06 +0100

    latest run files used in the manuscript

--- runs/lam_run.py
+++ runs/lam_run.py
@@ -3,20 +3,55 @@ from src import var
 
 params = var.params()
 
-params.fn_tag = 'lam_alaska'
+dfft_fa = False
+coarse = False
 
-params.lat_extent = [52.,64.,64.]
-params.lon_extent = [-141.,-158.,-127.]
+dfft_tag = 'dfft' if dfft_fa else 'lsff'
+coarse_tag = 'R2B4' if coarse else 'R2B5'
+
+params.fn_tag = 'lam_alaska_%s_fa_%s' %(dfft_tag, coarse_tag)
+
+if dfft_fa:
+    params.dfft_first_guess = True
+else:
+    params.dfft_first_guess = False
+
+params.lat_extent = [48.,64.,64.]
+params.lon_extent = [-148.,-148.,-112.]
 
 params.get_delaunay_triangulation = True
-params.delaunay_xnp = 16
-params.delaunay_ynp = 11
-# params.rect_set = np.sort([156,154,32,72,68,160,96,162,276,60])
+# corresponds to approx (160x160)km
 
-params.rect_set = np.sort([0, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 32, 34, 36, 38, 42, 44, 58, 60, 66, 68, 70, 72, 74, 76, 78, 80, 96, 98, 100, 102, 108, 110, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 150, 154, 156, 158, 160, 162, 168, 170, 172, 178, 180, 182, 184, 186, 188, 190, 192, 194, 216, 218, 224, 226, 228, 230, 232, 234, 244, 246, 250, 252, 254, 256, 258, 260, 264, 266, 272, 274, 276, 278, 280, 282, 294, 298])
-# rect_set = np.sort([156,154,32,72,160,96,162,276])
-# rect_set = np.sort([52,62,110,280,296,298,178,276,244,242])
-# rect_set = np.sort([276])
-params.lxkm, params.lykm = 120, 120
+if coarse:
+    params.delaunay_xnp = 14
+    params.delaunay_ynp = 11
+
+    params.lxkm, params.lykm = 160, 160
+
+    params.nhi = 32
+    params.nhj = 64
+    params.n_modes = 100
+else:
+    params.delaunay_xnp = 28
+    params.delaunay_ynp = 22
+
+    params.lxkm, params.lykm = 80, 80
+
+    params.nhi = 16
+    params.nhj = 32
+    params.n_modes = 50
+
+params.rect_set = np.sort([0,1,2,3])
+
+params.lmbda_fa = 1e-1 # first guess
+params.lmbda_sa = 1e-1 # second step
+
+params.U, params.V = 10.0, 0.0
 
 params.run_full_land_model = True
+
+params.padding = 10
+params.taper_ref = True
+params.taper_fa = True
+params.taper_sa = True
+params.taper_art_it = 20
--- runs/selected_run_dfft.py
+++ runs/selected_run_dfft.py
@@ -24,20 +24,37 @@ params.delaunay_ynp = 11
 # (xnp x ynp) = (14 x 11); (16, 11)
 params.rect_set = np.sort([20,148,160,212,38,242])
 
-params.nhi = 32
-params.nhj = 64
+# worst underestimators
+params.rect_set = np.sort([152,160,42,200,202,238,180])
+
+# worst overestimators
+params.rect_set = np.sort([92,24,28])
+
+# params.rect_set = np.sort([160])
+
+# params.nhi = 32
+# params.nhj = 64
+
+params.nhi = 12
+params.nhj = 24
+params.n_modes = 100
 
 # (xnp x ynp) = (16 x 14)
 # params.rect_set = np.sort([20,148,160,212,256,242])
 
 # corresponds to approx (80x80)km
-params.delaunay_xnp = 28
-params.delaunay_ynp = 22
-params.rect_set = np.sort([20,148,160,678,312,698, 342])
-params.nhi = 16
-params.nhj = 32
+# params.delaunay_xnp = 28
+# params.delaunay_ynp = 22
+# params.rect_set = np.sort([20,148,160,678,312,698, 342])
+# params.nhi = 16
+# params.nhj = 32
+# params.n_modes = 50
 # look into 342!!
 # params.rect_set = np.sort([342])
+# params.rect_set = np.sort([342,346,652,674,670,348,654,656])
+
+
+# params.rect_set = np.sort([598,908,906,902,896,898,622,902])
 
 # params.rect_set = np.sort([116])
 # all the main MERIT x10 offenders. To test implementation of correction strategy.
@@ -64,13 +81,11 @@ params.run_full_land_model = False
 
 params.padding = 10
 
-params.n_modes = 50
-
 params.dfft_first_guess = False
 
 params.taper_ref = True
 params.taper_fa = True
-params.taper_sa = False
+params.taper_sa = True
 params.taper_art_it = 20
 
 params.no_corrections = False
